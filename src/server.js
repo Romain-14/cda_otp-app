e0b812f6450d1e9e044f460f9617a18b2844dfa0
Romain-14 <romain14f@msn.com>
2024-10-11 00:55:52 +0200

    hotfix

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from './app.js';
 
-const PORT = process.env.LOCAL_PORT;
+const PORT = process.env.PORT || process.env.LOCAL_PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
